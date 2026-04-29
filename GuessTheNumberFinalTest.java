e71811223bbd19efb2fc35372e65c0dca8b5fd91
erican1214 <erican1214@outlook.com>
2026-04-28 21:44:21 -0400

    commiting junit tests

--- GuessTheNumberFinalTest.java
+++ GuessTheNumberFinalTest.java
@@ -4,22 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import jdk.jfr.Timestamp;
 
-public class EvenOddFinalTest {
-    EvenOdd evenOddGame = new EvenOdd();
+public class GuessTheNumberFinalTest {
+    GuessTheNumber guessTheNumberGame = new GuessTheNumber();
     GetInput getInputClass = new GetInput();
 
     @Test
     public void testValidGuess() {
         assertEquals(true, getInputClass.isInRange(77, 0, 100));
-    }
-
-    @Test
-    public void testCorrectSum() {
-        assertEquals(10, evenOddGame.getSum(5, 5));
-    }
-
-    @Test
-    public void testCorrectThrow() {
-        assertEquals(true, getInputClass.isInRange(3, 1, 5));
     }
 }
